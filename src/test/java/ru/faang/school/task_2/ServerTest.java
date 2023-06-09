a8a67a799af6d952a05cd733017d997bda5ff203
Alexander Kolesnikov <kolesnikov.kite@gmail.com>
2023-06-09 13:18:49 +0100

    added test folder

---
+++ src/test/java/ru/faang/school/task_2/ServerTest.java
@@ -0,0 +1,10 @@
+package ru.faang.school.task_2;
+
+import org.junit.jupiter.api.Test;
+
+public class ServerTest {
+
+    @Test
+    public void setLoadMethod_ShouldThrowIllegalArgumentException_ifLoadBiggerThanMaxLoad(){
+    }
+}
