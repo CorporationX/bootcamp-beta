e97e1710beb2ecf8c6b2a608f592b56db37a5e72
Alexander Kolesnikov <kolesnikov.kite@gmail.com>
2023-06-09 19:02:50 +0100

    Initial commit

---
+++ src/main/java/ru/faang/school/task_3/House.java
@@ -0,0 +1,15 @@
+package ru.faang.school.task_3;
+
+import lombok.Getter;
+
+@Getter
+public class House {
+
+    private String name;
+    private String sigil;
+
+    public House(String name, String sigil) {
+        this.name = name;
+        this.sigil = sigil;
+    }
+}
---
+++ src/main/java/ru/faang/school/task_3/HouseDirectory.java
@@ -0,0 +1,18 @@
+package ru.faang.school.task_3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HouseDirectory {
+
+    private Map<String, House> houses;
+
+    public void addHouse() {}
+
+    public void removeHouseByName(HashMap<String, House> houses, String name) {}
+
+    public void getHouseSigilByName(HashMap<String, House> houses, String name) {}
+
+    public void printListOfHouses(HashMap<String, House> houses) {}
+
+}
