e55fcfa97ff908665e7c822b314fd6850a050352
Pavel <pavlik1123.pg@gmail.com>
2023-06-11 14:59:21 +0700

    hashmap_task5

--- src/main/java/ru/faang/school/hashmap/task_5/Main.java
+++ src/main/java/ru/faang/school/hashmap/task_5/Main.java
@@ -2,50 +2,72 @@ package ru.faang.school.hashmap.task_5;
 
 import ru.faang.school.hashmap.task_5.webPage.WebPage;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class Main {
     private static final Map<String, List<WebPage>> mapContents = new HashMap<>();
 
     public static void main(String[] args) {
-        mapContents.put("функции", new ArrayList<>());
-        mapContents.put("создает", new ArrayList<>());
-        mapContents.put("факториала", new ArrayList<>());
+        mapContents.put("Пока", new ArrayList<>());
+        mapContents.put("быть", new ArrayList<>());
+        mapContents.put("ракета", new ArrayList<>());
 
-        addWebPageInList(new WebPage("qqq", "tns", "Рекурсивные тоже создает используют стек вызовов! Посмотрим, как это делается, на примере функции вычисления факториала. Вызов factorial(5) записывается в виде 5! и определяется следующим образом: 5! = 5*4*3*2*1. По тому же принципу factorial(3) соответствует 3*2*1. Рекурсивная создает функции функция для вычисления факториала числа выглядит так:"));
+        addWebPageInList(new WebPage("rrr", "port", lineFeedTo("Пока мы живы — можно все исправить,\n" +
+                "Пока возможно — полной грудью жить.\n" +
+                "Тех, кто не нужен, позади оставить.\n" +
+                "Кого не-до-любили — долюбить…")));
+        addWebPageInList(new WebPage("rrr", "port", lineFeedTo("Красивым быть — не значит им родиться,\n" +
+                "Ведь красоте мы можем научиться.\n" +
+                "Когда красив душою Человек —\n" +
+                "Какая внешность может с ней сравниться?")));
+        addWebPageInList(new WebPage("rrr", "port", lineFeedTo("Когда замрут отчаянье и злоба,\n" +
+                "Нисходит сон. И крепко спим мы оба\n" +
+                "На разных полюсах земли.\n" +
+                "\n" +
+                "Ты обо мне, быть может, грезишь в эти\n" +
+                "Часы. Идут часы походкою столетий,\n" +
+                "И сны встают в земной дали.")));
+        addWebPageInList(new WebPage("rrr", "port", lineFeedTo("Достигая желаемой высоты,\n" +
+                "Очень трудно быть собранным…На бегу,\n" +
+                "Перед тем, как поджечь за собой мосты,\n" +
+                "Убедись, а на том ли ты берегу.")));
 
-        addWebPageInList(new WebPage("rrr", "pimp", "Реализуйте метод для индексирования новой веб-страницы, который будет принимать объект класса WebPage и добавлять его в индекс. Метод должен разбивать содержание страницы н к переменной x, принадлежащей другой функции невозможно"));
+        searchList("Пока");
+        searchList("быть");
+        searchList("ракета");
 
-        try {
-//            searchKey("функции").forEach(webPage -> System.out.println(webPage.getContent()));
-            searchKey("создает").forEach(webPage -> System.out.println(webPage.getContent()));
-//            searchKey("факториала").forEach(webPage -> System.out.println(webPage.getContent()));
-        } catch (NullPointerException e) {
-            e.getMessage();
-        }
+    }
 
+    private static String lineFeedTo(String content) {
+        byte[] bytes = content.getBytes(StandardCharsets.UTF_8);
+        return new String(bytes, StandardCharsets.UTF_8);
     }
 
     private static void addWebPageInList(WebPage webPage) {
         String[] arrContent = webPage.getContent().split(" ", 0);
+        List<String> index = new ArrayList<>(50);
         for (int i = 0; i < arrContent.length; i++) {
-            Map<WebPage, String> webPageMap = new HashMap();
-            if (mapContents.containsKey(arrContent[i])) {
-                webPageMap.put(webPage, null);
-                if (!webPageMap.containsKey(webPage)) {
-                    mapContents.get(arrContent[i]).add(webPage);
-                }
+            if (mapContents.containsKey(arrContent[i]) & !index.contains(arrContent[i])) {
+                index.add(arrContent[i]);
+                mapContents.get(arrContent[i]).add(webPage);
             }
         }
     }
 
-    private static List<WebPage> searchKey(String key) {
-        if (mapContents.containsKey(key)) {
-            return mapContents.get(key);
+    private static void searchList(String key) {
+        Optional<List<WebPage>> list = searchOptional(key);
+        if (list.isPresent()) {
+            list.get().forEach(webPage -> System.out.println(key + ": \n" + webPage.getContent() + "_______________\n"));
+        } else {
+            System.out.printf("Лист %s пуст !!!", key);
         }
-        throw new NullPointerException("list null !!!");
+    }
+
+    private static Optional<List<WebPage>> searchOptional(String key) {
+        if (mapContents.containsKey(key) & !mapContents.get(key).isEmpty()) {
+            return Optional.of(mapContents.get(key));
+        }
+        return Optional.empty();
     }
 }
