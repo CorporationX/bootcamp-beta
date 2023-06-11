1463281a73dcd6ac78b214421e2f2f7cc045ca61
Pavel <pavlik1123.pg@gmail.com>
2023-06-11 21:14:45 +0700

    исправил ошибки hashmap_task3

--- src/main/java/ru/faang/school/hashmap/task_3/Main.java
+++ src/main/java/ru/faang/school/hashmap/task_3/Main.java
@@ -1,7 +1,7 @@
 package ru.faang.school.hashmap.task_3;
 
 
-import ru.faang.school.hashmap.task_3.student.GrupStudent;
+import ru.faang.school.hashmap.task_3.student.Grup;
 import ru.faang.school.hashmap.task_3.student.Student;
 
 import java.util.ArrayList;
@@ -10,10 +10,10 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    private static final List<Student> listStudents = new ArrayList<>();
+    private static final List<Student> students = new ArrayList<>();
 
     public static void main(String[] args) {
-        addStudent("pavel", "kiber", 1);
+        addStudent("pavel", "kiber", 5);
         addStudent("alexei", "kiber", 1);
         addStudent("sasha", "kiber", 4);
         addStudent("stepan", "freza", 1);
@@ -27,21 +27,26 @@ public class Main {
         allStudent();
     }
 
-    private static void addStudent(String name, String faculty, int year) {
-        listStudents.add(new Student(name, faculty, year));
+    private static void addStudent(String name, String faculty, int course) {
+        try {
+            students.add(new Student(name, faculty, course));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
     }
 
-    private static void deleteStudent(String name, String faculty, int year) {
-        listStudents.remove(new Student(name, faculty, year));
+    private static void deleteStudent(String name, String faculty, int course) {
+        students.remove(new Student(name, faculty, course));
     }
 
-    private static List<Student> searchAllStudent(String faculty, int year) {
-        GrupStudent grupStudent = new GrupStudent(faculty, year);
-        return getMapGrupFacultyYear().get(grupStudent);
+    private static List<Student> searchAllStudent(String faculty, int course) {
+        Grup grup = new Grup(faculty, course);
+        Map<Grup, List<Student>> students = getStudentPerGroups();
+        return students.get(grup);
     }
 
     private static void allStudent() {
-        getMapGrupFacultyYear().forEach((key, value) -> {
+        getStudentPerGroups().forEach((key, value) -> {
             System.out.println(key.toString());
             for (Student student : value) {
                 System.out.printf("%s \n", student.toString());
@@ -49,15 +54,15 @@ public class Main {
         });
     }
 
-    private static Map<GrupStudent, List<Student>> getMapGrupFacultyYear() {
-        Map<GrupStudent, List<Student>> mapGrupFacultyYear = new HashMap<>();
-        for (Student listStudent : listStudents) {
-            List<Student> studentsYearFaculty = new ArrayList<>();
-            if (!mapGrupFacultyYear.containsKey(listStudent.facultyYear())) {
-                mapGrupFacultyYear.put(listStudent.facultyYear(), studentsYearFaculty);
-                mapGrupFacultyYear.get(listStudent.facultyYear()).add(listStudent);
+    private static Map<Grup, List<Student>> getStudentPerGroups() {
+        Map<Grup, List<Student>> mapGrupFacultyYear = new HashMap<>();
+        for (Student student : students) {
+            Grup grup = student.getFacultyYear();
+            if (!mapGrupFacultyYear.containsKey(grup)) {
+                mapGrupFacultyYear.put(grup, new ArrayList<>());
+                mapGrupFacultyYear.get(grup).add(student);
             } else {
-                mapGrupFacultyYear.get(listStudent.facultyYear()).add(listStudent);
+                mapGrupFacultyYear.get(grup).add(student);
             }
         }
         return mapGrupFacultyYear;
